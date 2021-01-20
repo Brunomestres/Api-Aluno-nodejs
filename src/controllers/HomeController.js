24024ee67677c8a57e4d6ddea4364492c2de72fc
Bruno <brunomestres@gmail.com>
2021-01-20 14:07:07 -0300

    CRUD Alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -2,16 +2,20 @@ import Aluno from '../models/Aluno';
 
 class HomeController {
   async index(req, res) {
-    const aluno = await Aluno.create({
-      nome: 'Bruno',
-      sobrenome: 'Mestres',
-      email: 'bruno@',
-      idade: 21,
-      peso: 25.6,
-      altura: 35.6,
-    });
+    try {
+      const aluno = await Aluno.create({
+        nome: 'Bruno',
+        sobrenome: 'Mestres',
+        email: 'bruno@',
+        idade: 21,
+        peso: 25.6,
+        altura: 35.6,
+      });
 
-    res.json(aluno);
+      res.json(aluno);
+    } catch (error) {
+      res.status(401).json('Deu erro');
+    }
   }
 }
 
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,13 +2,76 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const user = await User.create({
-      nome: 'Bruno',
-      email: 'bruno@gmail.com',
-      password: '123456',
-    });
+    try {
+      const user = await User.create(req.body);
 
-    res.json(user);
+      return res.json(user);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuario não existe'],
+        });
+      }
+      const newUser = await user.update(req.body);
+
+      return res.json(newUser);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuario não existe'],
+        });
+      }
+      await user.destroy(req.body);
+
+      return res.json(user);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
   }
 }
 
